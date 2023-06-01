f32a09327a60ae837a7706718cbbf02ef33dde41
Hamit <sadecehamit@gmail.com>
2023-06-01 22:13:52 +0200

    28.05.2023

---
+++ src/test/java/TeamPackage/day04/C02_Task2.java
@@ -0,0 +1,14 @@
+package TeamPackage.day04;
+
+public class C02_Task2 {
+    public static void main(String[] args) {
+      // https://www.amazon.com/ adresine gidin  3- Browseri tam sayfa yapin
+        // Sayfayi "refresh" yapin
+        //Sayfa basliginin "Spend less" ifadesi icerdigini test edin
+        //Gift Cards sekmesine basin
+        //Birthday butonuna basin
+        //Best Seller bolumunden ilk urunu tiklayin  9- Gift card details'den 25 $'i secin
+        //10-Urun ucretinin 25$ oldugunu test edin
+       // 10-Sayfayi kapatin
+    }
+}
