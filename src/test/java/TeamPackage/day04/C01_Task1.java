af87e035c76c7bed8a91cd1af4d13ed6b12c007e
Hamit <sadecehamit@gmail.com>
2023-06-01 22:11:23 +0200

    28.05.2023

---
+++ src/test/java/TeamPackage/day04/C01_Task1.java
@@ -0,0 +1,12 @@
+package TeamPackage.day04;
+
+public class C01_Task1 {
+    public static void main(String[] args) {
+        // http://the-internet.herokuapp.com/add_remove_elements/ adresine gidiniz
+        // Add Element butonuna 100 defa basınız
+        // 100 defa basıldığını test ediniz
+        // 90 defa delete butonuna basınız
+        // 90 defa basıldığını doğrulayınız
+        // Sayfayı kapatınız
+    }
+}
